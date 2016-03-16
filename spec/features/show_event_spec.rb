e0a9f3610d917400ca1a1e685519edb2ed26f84e
Philip Ainza <philainza@gmail.com>
2016-03-16 14:22:26 -0700

    Add navigation specs

--- spec/features/show_event_spec.rb
+++ spec/features/show_event_spec.rb
@@ -3,11 +3,7 @@ require 'rails_helper'
 describe "Viewing an individual event" do 
 
   it "shows the event's details" do
-    event = Event.create(name: "Bugsmash",
-                        location: "Denver",
-                        price: 10.00,
-                        description: 'Fun evening of bug smashing!',
-                        starts_at: 10.days.from_now)
+    event = Event.create(event_attributes(price: 10.00))
 
     visit event_url(event)
 
